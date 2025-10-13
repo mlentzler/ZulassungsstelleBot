359c086e775f694335c3b3d22a818874fdec31d4
mlentzler <michel.lentzler@gmail.com>
2025-10-13 15:46:48 +0200

    Implements menug navigation functionality

--- internal/tui/menu.go
+++ internal/tui/menu.go
@@ -1,20 +1,56 @@
 package tui
 
 import (
+	"strings"
+
 	"github.com/mlentzler/ZulassungsstelleBot/internal/domain"
 )
 
 func currentLevelNodes(m *Model) []domain.MenuNode {
-	// TODO: aus m.menuRoot & m.menuStack die aktuelle Ebene ermitteln
-	return nil
+	node := m.menuRoot
+	for _, i := range m.menuStack {
+		if i < 0 || i >= len(node.Children) {
+			return nil
+		}
+		node = node.Children[i]
+	}
+	return node.Children
 }
 
-func pushMenu(m *Model, idx int) {
-	// TODO: ausgewählten Title in m.path pushen, neue Ebene öffnen
+func currentPathTitles(m *Model) []string {
+	var titles []string
+	node := m.menuRoot
+	for _, i := range m.menuStack {
+		if i < 0 || i >= len(node.Children) {
+			return titles
+		}
+		node = node.Children[i]
+		titles = append(titles, node.Title)
+	}
+	return titles
+}
+
+func breadcrumb(m *Model) string {
+	parts := []string{"Start"}
+	parts = append(parts, currentPathTitles(m)...)
+	return strings.Join(parts, " > ")
+}
+
+func pushMenu(m *Model, i int) {
+	nodes := currentLevelNodes(m)
+	if i < 0 || i >= len(nodes) {
+		return
+	}
+	m.menuStack = append(m.menuStack, i)
+	m.menuCursor = 0
 }
 
 func popMenu(m *Model) {
-	// TODO: eine Ebene zurück (stack & path anpassen)
+	if len(m.menuStack) == 0 {
+		return
+	}
+	m.menuStack = m.menuStack[:len(m.menuStack)-1]
+	m.menuCursor = 0
 }
 
 // updateMenu(m, msg) & viewMenu(m) bitte in model.go / view.go aufrufen
